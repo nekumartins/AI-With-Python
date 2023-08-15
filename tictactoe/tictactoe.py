2e94c8f15dd6aee40927f1a54ca7e4dbdf2db827
Riley <akpotohwoo@gmail.com>
2023-08-15 20:50:06 +0100

    update

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -56,32 +56,88 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    newboard = deepcopy(board)
+    if newboard[action[0]][action[1]] != EMPTY:
+        raise Exception("Invalid Move")
+    else:
+        newboard[action[0]][action[1]] = player(newboard)
+        return newboard
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    for row in range(3):
+        if board[row][0] == board[row][1] == board[row][2] != EMPTY:
+            return board[row][0]
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] != EMPTY:
+            return board[0][col]
+    if board[0][0] == board[1][1] == board[2][2] != EMPTY:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != EMPTY:
+        return board[0][2]
+    return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if winner(board) != None:
+        return True
+    for row in range(3):
+        for col in range(3):
+            if board[row][col] == EMPTY:
+                return False
+    return True
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    winner = winner(board)
+    if winner == X:
+        return 1
+    elif winner == O:
+        return -1
+    else:
+        return 0
 
+def minvalue(board):
+    if terminal(board):
+        return utility(board)
+    value = math.inf
+    for action in actions(board):
+        value = min(value, maxvalue(result(board, action)))
+    return value
+def maxvalue(board):
+    if terminal(board):
+        return utility(board)
+    value = -math.inf
+    for action in actions(board):
+        value = max(value, minvalue(result(board, action)))
+    return value
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    currentplayer = player(board)
+    if currentplayer == X:
+        value = -math.inf
+        for action in actions(board):
+            newvalue = minvalue(result(board, action))
+            if newvalue > value:
+                value = newvalue
+                bestaction = action
+    elif currentplayer == O:
+        value = math.inf
+        for action in actions(board):
+            newvalue = maxvalue(result(board, action))
+            if newvalue < value:
+                value = newvalue
+                bestaction = action
+    return bestaction
--- traffic/traffic.py
+++ traffic/traffic.py
@@ -58,7 +58,20 @@ def load_data(data_dir):
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
     """
-    raise NotImplementedError
+    
+        
+    images = []
+    labels = []
+        
+    for i in range(NUM_CATEGORIES):
+        path = os.path.join(data_dir, str(i))
+        for file in os.listdir(path):
+                img = cv2.imread(os.path.join(path, file))
+                img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+                images.append(img)
+                labels.append(i)
+                
+    return (images, labels)
 
 
 def get_model():
@@ -67,7 +80,29 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+        
+    model = tf.keras.models.Sequential([
+            tf.keras.layers.Conv2D(
+                32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+            ),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            tf.keras.layers.Conv2D(
+                64, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+            ),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dropout(0.5),
+            tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+        ])
+        
+    model.compile(
+            optimizer="adam",
+            loss="categorical_crossentropy",
+            metrics=["accuracy"]
+        )
+        
+    return model
 
 
 if __name__ == "__main__":
